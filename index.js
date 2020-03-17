89538fcb18d398758a95ce7f06728a0f7d9806d4
Artem_Zabirov <qwertysairex@gmail.com>
2020-03-17 23:23:27 +0300

    improved(added discribe app, added ability to select entry and exit point, and delete entry dir)

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const fs = require("fs");
+const yargs = require("yargs");
 const path = require("path");
 const Events = require("events");
-
-const { outputPath, inputPath } = {
-    outputPath: path.join(__dirname, "src"),
-    inputPath: path.join(__dirname, "dist")
+const basicPaths = {
+    source: null,
+    dist: null
 };
+const argv = yargs
+    .usage("Usage: $0 [option]")
+    .help("help")
+    .alias("help", "h")
+    .version("0.0.1")
+    .alias("version", "v")
+    .example("$0 --entry ./filesSort --output ./dist -D => Sortings folder")
+    .option("entry", {
+        alias: "e",
+        describe: "Путь к читаемой директории",
+        demandOption: true
+    })
+    .option("output", {
+        alias: "o",
+        describe: "Путь куда сортировать файлы",
+        default: "./dist"
+    })
+    .option("delete", {
+        alias: "D",
+        describe: "Необходимо удалить исходную директорию после сортировки?",
+        default: false
+    })
+    .epilog("App for week_1").argv;
+
+basicPaths.source = path.normalize(path.join(__dirname, argv.entry));
+basicPaths.dist = path.normalize(path.join(__dirname, argv.output));
+
 const eventEmitter = new Events();
 
 eventEmitter.on("observeEmptyDir", dir => {
-    fs.readdir(dir, (err, data) => {
-        err && console.log("READEMIT", err);
-        if (fs.existsSync(dir)) {
-            if (!data.length) {
-                fs.rmdir(dir, err => {
-                    err && console.log("RMDIR", err);
-                    const faterPath = path.parse(dir).dir;
-                    eventEmitter.emit("observeEmptyDir", faterPath);
-                });
+    argv.delete === "true" &&
+        fs.readdir(dir, (err, data) => {
+            err && console.log("READEMIT", err);
+            if (fs.existsSync(dir)) {
+                if (!data.length) {
+                    fs.rmdir(dir, err => {
+                        err && console.log("RMDIR", err);
+                        const faterPath = path.parse(dir).dir;
+                        eventEmitter.emit("observeEmptyDir", faterPath);
+                    });
+                }
             }
-        }
-    });
+        });
 });
 const moveFile = (file, dir, currentPath, pathToDir) => {
     const inPath = path.join(dir, file);
@@ -36,10 +64,10 @@ const moveFile = (file, dir, currentPath, pathToDir) => {
     });
 };
 const contorolLocationFile = (file, currentPath, pathToDir) => {
-    const dir = path.join(inputPath, file[0].toUpperCase());
-    if (!fs.existsSync(path.join(inputPath, `${file[0]}`))) {
+    const dir = path.join(basicPaths.dist, file[0].toUpperCase());
+    if (!fs.existsSync(path.join(basicPaths.dist, `${file[0]}`))) {
         fs.mkdir(dir, err => {
-            err && console.log(err);
+            err && console.log("MKLITER", err);
             moveFile(file, dir, currentPath, pathToDir);
         });
     } else {
@@ -50,10 +78,10 @@ const searchFile = (data, rootPath) => {
     data.forEach(elem => {
         const pathToElem = path.join(rootPath, elem);
         fs.lstat(pathToElem, (err, stats) => {
-            err && console.log(err);
+            err && console.log("LSTAT", err);
             if (stats.isDirectory()) {
                 fs.readdir(pathToElem, (err, files) => {
-                    err && console.log(err);
+                    err && console.log("READFOREACH", err);
                     searchFile(files, pathToElem);
                 });
             }
@@ -61,10 +89,11 @@ const searchFile = (data, rootPath) => {
         });
     });
 };
-fs.readdir(outputPath, (err, data) => {
-    err && console.log(err);
-    fs.mkdir(inputPath, err => {
-        err && console.log(err);
-        searchFile(data, outputPath);
-    });
+argv.output === "./dist" &&
+    !fs.existsSync(path.join(__dirname, "dist")) &&
+    fs.mkdir(basicPaths.dist, err => err && console.log("MKDIST", err));
+
+fs.readdir(basicPaths.source, (err, data) => {
+    err && console.log("READBASIC", err);
+    searchFile(data, basicPaths.source);
 });
